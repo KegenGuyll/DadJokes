107f67d746deea819829c133fef0714b1de4d90b
Kegen Guyll <guyllkegen@gmail.com>
2019-07-08 20:53:33 -0500

    first commit

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+  Button,
+  Card,
+  Container,
+  Row,
+  Col,
+  Modal,
+  ListGroup,
+  ListGroupItem
+} from 'react-bootstrap';
+import '../../index.css';
+import Navigation from '../navbar';
+import { FiHeart, FiShare } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
 
 class Joke extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      joke: '',
+      setup: '',
       punchline: '',
-      jokeId: ''
+      jokeId: '',
+      share: false,
+      shareUrl: ''
     };
   }
 
@@ -17,28 +33,91 @@ class Joke extends Component {
 
   loadJoke = () => {
     axios
-      .get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' }
-      })
+      .get(
+        'https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random_joke',
+        {
+          headers: { Accept: 'application/json' }
+        }
+      )
       .then(response => {
         this.setState({
           jokeId: response.data.id,
-          joke: response.data.joke
+          setup: response.data.setup,
+          punchline: response.data.punchline
         });
       });
   };
 
+  handleShare = () => {
+    this.setState(preState => ({
+      share: !preState.share,
+      shareUrl: ``
+    }));
+  };
+
   render() {
     return (
-      <div
-        style={{
-          width: '100%',
-          textAlign: 'center'
-        }}
-      >
-        <h1>Dad Jokes</h1>
-        {this.state.jokeId ? <p>{this.state.joke}</p> : null}
-        <button onClick={this.loadJoke}>New Joke</button>
+      <div>
+        <Navigation />
+        <Container>
+          <Row>
+            <Col>
+              <div style={{ height: '25vh' }} />
+            </Col>
+          </Row>
+          <Row className='justify-content-md-center'>
+            <Col xs lg='4'>
+              <Card bg='dark' text='white'>
+                <Card.Header>
+                  <h4>Father Jokes</h4>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    {this.state.jokeId ? this.state.setup : null}
+                  </Card.Title>
+                  <Card.Text>
+                    {this.state.jokeId ? this.state.punchline : null}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <div style={{ float: 'right' }}>
+                    <IconContext.Provider
+                      value={{ color: 'white', size: '1.5em' }}
+                    >
+                      <FiShare onClick={this.handleShare} />
+                    </IconContext.Provider>
+                    <IconContext.Provider
+                      value={{ color: 'white', size: '1.5em' }}
+                    >
+                      <FiHeart />
+                    </IconContext.Provider>
+                  </div>
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <Modal centered show={this.state.share} onHide={this.handleShare}>
+          <Modal.Header closeButton>
+            <Modal.Title id='contained-modal-title-vcenter'>Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <RedditShareButton
+                  title={this.state.setup}
+                  url={`https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/jokes/${
+                    this.state.jokeId
+                  }`}
+                />
+              </ListGroupItem>
+              <ListGroupItem>hi</ListGroupItem>
+            </ListGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleShare}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
