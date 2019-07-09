e441d5cd32dbe7454add7fbaa4f42731de7a009b
unknown <hamiltonjlewis@gmail.com>
2019-07-08 21:50:39 -0500

    punchline hidden

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Button, Jumbotron, Container} from 'react-bootstrap';
+import {Button, Jumbotron, Container, Accordion, Card} from 'react-bootstrap';
 
 class Joke extends Component {
   constructor(props) {
@@ -34,8 +34,16 @@ class Joke extends Component {
       <Jumbotron fluid>
         <Container>
           <h1>Dad Jokes</h1>
-          {this.state.setup ? <p>{this.state.setup}</p> : null}
-          {this.state.punchline ? <p>{this.state.punchline}</p> : null}
+          <Accordion>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+              {this.state.setup ? <p>{this.state.setup}</p> : null}
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>{this.state.punchline ? <p>{this.state.punchline}</p> : null}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
           <Button onClick={this.loadJoke} variant="primary" size="lg">New Joke</Button>
         </Container>
       </Jumbotron>
