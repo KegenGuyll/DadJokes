1d70006b390c0c8e964eec07a586f4a41af6592d
unknown <hamiltonjlewis@gmail.com>
2019-07-11 21:19:32 -0500

    finished prototype

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Button, Jumbotron, Container, Accordion, Card} from 'react-bootstrap';
+import {Button, Container, Row, Col, Accordion, Card} from 'react-bootstrap';
 import "./index.css";
 
 class Joke extends Component {
@@ -19,7 +19,7 @@ class Joke extends Component {
 
   loadJoke = () => {
     axios
-      .get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random_joke', {
+      .get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes', {
         headers: { Accept: 'application/json' }
       })
       .then(response => {
@@ -32,19 +32,31 @@ class Joke extends Component {
 
   render() {
     return (
-      <Card style={{ background: '#6B5B7B', color: 'white'}}>
-        <Card.Body>
-          <Card.Title>Dad Jokes</Card.Title>
-          <Accordion>
-          {this.state.setup ? <Card.Text>{this.state.setup}</Card.Text> : null}
-          <Accordion.Collapse eventKey="0">
-                <Card.Text>{this.state.punchline ? <p>{this.state.punchline}</p> : null}</Card.Text>
-          </Accordion.Collapse>
-          <Accordion.Toggle as={Button}  style={{ background: '#F7B195', border: 'none'}} size="lg" eventKey="0" block>Reveal Punchline</Accordion.Toggle>
-          <Button onClick={this.loadJoke} style={{ background: '#F7B195', border: 'none'}} size="lg" block>New Joke</Button>
-          </Accordion>
-        </Card.Body>
-      </Card>
+
+      <Container>
+        <Row>
+            <Col>
+              <div style={{ height: '25vh' }} />
+            </Col>
+        </Row>
+        <Row className='justify-content-md-center'>
+          <Col xs lg='4'>
+          <Card style={{margin: '1rem', background: '#345B7C', color: 'white'}}>
+            <Card.Body>
+              <Card.Title>Dad Jokes</Card.Title>
+              <Accordion>
+              {this.state.setup ? <Card.Text>{this.state.setup}</Card.Text> : null}
+              <Accordion.Collapse eventKey="0">
+                    <Card.Text>{this.state.punchline ? <p>{this.state.punchline}</p> : null}</Card.Text>
+              </Accordion.Collapse>
+              <Accordion.Toggle as={Button}  style={{ background: '#F67280', border: 'none'}} size="lg" eventKey="0" block>Reveal Punchline</Accordion.Toggle>
+              <Button onClick={this.loadJoke} style={{ background: '#F67280', border: 'none'}} size="lg" block>New Joke</Button>
+              </Accordion>
+            </Card.Body>
+          </Card>
+          </Col>
+          </Row>
+      </Container>
     );
   }
 }
