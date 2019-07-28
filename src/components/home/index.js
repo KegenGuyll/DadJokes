5a384baa956c086362f68080fe64760ed095cd1d
Kegen Guyll <guyllkegen@gmail.com>
2019-07-28 08:06:09 -0500

    started work on swipe

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Button, Container, Row, Col, Accordion, Card} from 'react-bootstrap';
-import "./index.css";
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './index.css';
+import Swipe from 'ui-react.swipe';
 
 class Joke extends Component {
   constructor(props) {
@@ -9,7 +10,10 @@ class Joke extends Component {
     this.state = {
       setup: '',
       punchline: '',
-      jokeId: ''
+      jokeId: '',
+      jokeArray: [],
+      showJoke: false,
+      cardBackground: '#345B7C'
     };
   }
 
@@ -17,46 +21,99 @@ class Joke extends Component {
     this.loadJoke();
   }
 
+  onSwipeStart = () => {
+    this.setState({
+      cardBackground: 'rgba(62, 111, 152, 0.45)'
+    });
+  };
+
+  onSwipeLeft = () => {
+    const maxNumber = this.state.jokeArray.length;
+    const randomNumber = Math.floor(Math.random() * maxNumber);
+    this.state.jokeArray.map((joke, index) => {
+      if (index === randomNumber) {
+        this.setState({
+          setup: joke.setup,
+          punchline: joke.punchline
+        });
+        if (this.state.jokeArray.length !== 1) {
+          this.state.jokeArray.splice(index, 1);
+          this.handleShowJoke();
+          this.setState({
+            cardBackground: '#345B7C'
+          });
+        } else {
+          console.log('loaded data');
+          this.loadJoke();
+        }
+      }
+    });
+  };
+
+  handleShowJoke = () => {
+    this.setState(prevState => ({
+      showJoke: !prevState.showJoke
+    }));
+  };
+
   loadJoke = () => {
     axios
-      .get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes', {
-        headers: { Accept: 'application/json' }
-      })
+      .get(
+        'https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes/5',
+        {
+          headers: { Accept: 'application/json' }
+        }
+      )
       .then(response => {
         this.setState({
-          setup: response.data.setup,
-          punchline: response.data.punchline
+          jokeArray: response.data
         });
+      })
+      .then(() => {
+        this.setState(prevState => ({
+          setup: prevState.jokeArray[0].setup,
+          punchline: prevState.jokeArray[0].punchline
+        }));
+        this.state.jokeArray.splice(0, 1);
       });
   };
 
   render() {
     return (
-
       <Container>
         <Row>
-            <Col>
-              <div style={{ height: '25vh' }} />
-            </Col>
+          <Col>
+            <div style={{ height: '25vh' }} />
+          </Col>
         </Row>
         <Row className='justify-content-md-center'>
           <Col xs lg='4'>
-          <Card style={{margin: '1rem', background: '#345B7C', color: 'white'}}>
-            <Card.Body>
-              <Card.Title>Dad Jokes</Card.Title>
-              <Accordion>
-              {this.state.setup ? <Card.Text>{this.state.setup}</Card.Text> : null}
-              <Accordion.Collapse eventKey="0">
-                    <Card.Text>{this.state.punchline ? <p>{this.state.punchline}</p> : null}</Card.Text>
-              </Accordion.Collapse>
-              <Accordion.Toggle as={Button} style={{ background: '#F67280', border: 'none'}} size="lg" eventKey="0" block>Reveal Punchline</Accordion.Toggle>
-              <Accordion.Toggle as={Button} onClick={this.loadJoke} style={{ background: '#F67280', border: 'none'}} size="lg" eventKey="1" block>New Joke</Accordion.Toggle>
-             
-              </Accordion>
-            </Card.Body>
-          </Card>
+            <Swipe
+              axis='x'
+              onSwipeStart={this.onSwipeStart}
+              onSwipeLeft={this.onSwipeLeft}
+            >
+              <Card
+                onClick={this.handleShowJoke}
+                style={{
+                  margin: '1rem',
+                  background: this.state.cardBackground,
+                  color: 'white'
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Dad Jokes</Card.Title>
+                  {this.state.setup ? (
+                    <Card.Text>{this.state.setup}</Card.Text>
+                  ) : null}
+                  <Card.Text>
+                    {this.state.showJoke ? this.state.punchline : null}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Swipe>
           </Col>
-          </Row>
+        </Row>
       </Container>
     );
   }
